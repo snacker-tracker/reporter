e7d485416629537a90ba17c060adcbba9dbf8b1d
Laurence MacGuire <leprechaun@gmail.com>
2019-12-08 01:34:47 +0700

    Add the missing operations (some are broken)

---
+++ src/operations/CreateCodePicture.js
@@ -0,0 +1,65 @@
+import { Operation, HTTPResponse } from '../lib/operation'
+import CreateOperation from '../lib/CreateOperation'
+
+import crypto from 'crypto'
+
+import { Code } from '../models'
+
+import uuid from 'uuid'
+
+class CreateCodePicture extends Operation {
+  static model = Code
+
+  async extract_params(req) {
+    const d = new Date().toISOString()
+    this.args = {
+      body: {
+        ...req.params,
+        created_at: d,
+        updated_at: d
+      },
+      picture: req.files[0],
+      base_url: [
+        req.headers['x-forwarded-proto'] || 'http',
+        '://',
+        req.headers['x-forwarded-host'] || req.headers['host'],
+        req.originalUrl
+      ].join('')
+
+    }
+  }
+
+  async execute() {
+    let hash = crypto.createHash('sha256').update(this.args.picture.buffer).digest('hex');
+
+    try {
+      await this.services.image_repository.put([this.args.body.code, hash].join('/') + "." + this.args.picture.mimetype.split('/')[1], this.args.picture.buffer)
+    } catch(error) {
+      console.log(error)
+    }
+
+    const picture = {
+      id: hash,
+      url: this.args.base_url + '/' + hash + '.' + this.args.picture.mimetype.split('/')[1],
+      size: this.args.picture.size,
+      last_modified: new Date().toISOString()
+    }
+
+
+    this.services.event_publisher.publish(
+      ['CodePicture', 'Created'].join(''),
+      picture
+    )
+
+
+    return new HTTPResponse({
+      status: 201,
+      body: picture
+    })
+  }
+
+}
+
+export {
+  CreateCodePicture
+}
---
+++ src/operations/GetScanHistory.js
@@ -0,0 +1,27 @@
+import ListOperation from '../lib/ListOperation'
+import { Scan } from '../models'
+
+class ListScans extends ListOperation {
+  static model = Scan
+  static hasDeletion = false
+
+  extract_params(req, res) {
+    this.args = {
+      code: req.query.code,
+      from_date: req.query.from_date,
+      to_date: req.query.to_date,
+      interval: '1 day'
+    }
+  }
+
+  async execute() {
+    const q = this.constructor.model.query()
+
+    q.select('code', '')
+  }
+
+}
+
+export {
+  ListScans
+}
