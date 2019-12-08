a3d3d49829d4c603c60c56c445f2facb19c675b5
Laurence MacGuire <leprechaun@gmail.com>
2019-12-08 22:44:23 +0700

    Add a PatchOperation primitive and PatchCode operation

--- src/operations/PatchCode.js
+++ src/operations/PatchCode.js
@@ -1,12 +1,21 @@
-import GetOperation from '../lib/GetOperation'
+import PatchOperation from '../lib/PatchOperation'
 import { Code } from '../models'
 
-class PatchCode extends GetOperation {
+class PatchCode extends PatchOperation {
   static model = Code
+
+  getId() {
+    return this.args.id
+  }
+
+  getPatch() {
+    return this.args.body
+  }
 
   async extract_params(req) {
     this.args = {
-      id: req.params.code
+      id: req.params.code,
+      body: req.body
     }
   }
 }
