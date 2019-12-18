a44090c2acc697f8e77951d9d135f83fffc430b4
Laurence MacGuire <leprechaun@gmail.com>
2019-12-19 00:38:55 +0700

    A semi working stream-processor (uploads pictures too!)

--- src/lib/operation_to_handler.js
+++ src/lib/operation_to_handler.js
@@ -14,43 +14,6 @@ if(Config.s3) {
   s3_bucket = Config.s3.bucket
 }
 
-class ImageRepository {
-  constructor(s3client, bucket) {
-    this.client = s3client
-    this.bucket = bucket
-  }
-
-  async put(key, readable) {
-    const args = {
-      Bucket: this.bucket,
-      Key: key,
-      Body: readable
-    }
-
-    return this.client.upload(args).promise()
-  }
-
-  async list(prefix) {
-    const params = {
-      Bucket: this.bucket,
-      Prefix: prefix
-    }
-
-    return this.client.listObjects(params).promise()
-  }
-
-  presign(key) {
-    const args = {
-      Bucket: this.bucket,
-      Key: key,
-      Expires: 300
-    }
-
-    return this.client.getSignedUrl('getObject', {
-      ...args
-    })
-  }
-}
 
 const IR = new ImageRepository(s3_client, s3_bucket)
 
--- src/stream.js
+++ src/stream.js
@@ -3,12 +3,19 @@ import uuid from 'uuid'
 import config from './config/'
 import axios from 'axios'
 
+
+import crypto from 'crypto'
+
+import ImageRepository from './lib/ImageRepository'
+
 import logger from './lib/logger'
 
 import express from 'express'
 const server = express()
 
 import prom from 'prom-client'
+
+import InfoStores from './lib/ProductInfoStores'
 
 const register = prom.register
 server.get('/metrics', (req, res) => {
@@ -17,7 +24,6 @@ server.get('/metrics', (req, res) => {
 });
 
 server.listen(config.port);
-
 
 //import metrics from './lib/metrics'
 
@@ -69,74 +75,58 @@ class CodePictureCreatedHandler extends EventHandler{
 
 
 class ScanCreatedHandler extends EventHandler{
-  async getCode(code) {
-    try {
-      const response = await axios.get(
-        config.reporter_base_url + '/codes/' + code
-      )
-
-      return response.data
-    } catch( error ) {
-      return false
-    }
-  }
-
-  async getFromOpenFoodFacts(code) {
-    // https://world.openfoodfacts.org/api/v0/product/8850999220000.json
-    try {
-      const response = await axios.get(
-        "https://world.openfoodfacts.org/api/v0/product/" + code + ".json"
-      )
-
-      return response.data
-    } catch( error ) {
-      return false
-    }
-
-  }
-
-  async getFromUPCDB(code) {
-    try {
-      const response = await axios.get(
-        'https://api.upcitemdb.com/prod/trial/lookup',
-        {
-          params: {
-            upc: code
-          }
-        }
-      )
-
-      return response.data
-    } catch(error) {
-      return false
-    }
-
-  }
-
-
   async run({id, event, timestamp, payload, version, actor}) {
     this.services.logger.info({event_handler: this.constructor.name, event, event_id: id, timestamp, version, payload, actor })
 
-    //const code_data = await this.getCode(payload.code)
-    //this.services.logger.info({"msg":"code data", code_data})
+    const local = await this.services.productInfoStores.snacker.get(payload.code)
+    const off = await this.services.productInfoStores.off.get(payload.code)
 
-    // $.product.image_front_url
-    const off_data = await this.getFromOpenFoodFacts(payload.code)
-    console.log(JSON.stringify(off_data, null, 2))
-    //this.services.logger.info({"msg":"off data", off_data})
-    if(off_data) {
-      if(off_data.product) {
-        if(off_data.product.image_url) {
-          console.log(off_data.product.image_url)
+    const firstOf = (options, hash) => {
+      for(const option of options) {
+        if(hash[option] != undefined && hash[option] != "" && hash[options] != "unknown") {
+          return hash[option]
         }
       }
     }
 
-    //const upcdb_data = await this.getFromUPCDB(payload.code)
 
+    if(off) {
+      //console.log(JSON.stringify(Object.keys(off.product), null, 2))
+      let p = off.product
 
+      const data = {
+        code: payload.code,
+        name: firstOf(['product_name_en', 'generic_name_en', 'product_name', 'generic_name', 'product_name_th', 'generic_name_th'], off.product)
+      }
 
-    return "one"
+      try {
+        const postResponse = await this.services.productInfoStores.snacker.post(data)
+        //console.log(postResponse)
+      } catch (error) {
+        //console.log(error)
+      }
+
+      console.log(p.image_url)
+
+      if(p.image_url) {
+        const img_response = await axios.get(p.image_url, {responseType: 'arraybuffer'})
+        logger.info({headers: img_response.headers})
+
+        const extension = img_response.headers['content-type'].split('/')[1]
+
+        let hash = crypto.createHash('sha256').update(img_response.data).digest('hex')
+
+        try {
+          await this.services.productInfoStores.snacker.post_picture(payload.code, img_response.data)
+          this.services.logger.info("Image uploaded")
+        } catch(error) {
+          console.log(error)
+        }
+
+      }
+    }
+
+    return true
   }
 }
 
@@ -212,7 +202,25 @@ class KinesisConsumer {
     }
 
     const instances = this.handlers[event.event].map( handler => {
-      return new handler({logger: l})
+      const IR = new ImageRepository(new AWS.S3(config.s3), config.s3.bucket)
+
+      const l = new logger.constructor(logger.instance)
+
+      l.setContext('event', event.event)
+      l.setContext('event_id', event.id)
+      l.setContext('handler', handler.name)
+
+
+      return new handler({
+        logger: l,
+        image_repository: IR,
+        productInfoStores: {
+          bigc: new InfoStores.BigCInfoStore(),
+          upcdb: new InfoStores.UPCItemDBInfoStore(),
+          off: new InfoStores.OpenFoodFactsInfoStore(),
+          snacker: new InfoStores.SnackerTrackerInfoStore(config.reporter_base_url)
+        }
+      })
     })
 
     let results = await Promise.all(
@@ -234,264 +242,14 @@ class KinesisConsumer {
       })
     )
 
-    results = results.reduce((a, c) => {
-      a[Object.entries(c)[0][0]] = Object.entries(c)[0][1]
-    }, {})
-
-    l.info(results)
+    for(const result of results) {
+      l.info({
+        handler: Object.entries(result)[0][0],
+        result: Object.entries(result)[0][1]
+      })
+    }
 
     return true
-  }
-}
-
-
-// curl -v "https://www.bigc.co.th/sb.php?q=${UPC}&storeid=1&currencycode=THB" -H 'X-Requested-With: XMLHttpRequest'  | jq .response
-
-class MyConsumerBloated extends KinesisConsumer {
-
-  async check_local(code) {
-    let local_code
-    try {
-      local_code = await axios.get(
-        config.reporter_base_url + '/codes/' + code
-      )
-
-      console.log(JSON.stringify(local_code.data, null, 2))
-      return local_code.data
-    } catch(error) {
-      console.log({
-        message: 'read local ...',
-        status: error.response.status,
-        headers: error.response.headers
-      })
-
-      return false
-    }
-  }
-
-  async read_upcitemdb(code) {
-    let upc_search_result
-    try {
-      upc_search_result = await axios.get(
-        'https://api.upcitemdb.com/prod/trial/lookup',
-        {
-          params: {
-            upc: record.payload.code
-          }
-        }
-      )
-
-      console.log(JSON.stringify(upc_search_result.headers, null, 2))
-      console.log(JSON.stringify(upc_search_result.data, null, 2))
-
-    } catch(error) {
-      console.log("Failed to query the UPC database")
-      return false
-    }
-
-    if(upc_search_result.status != 200) {
-      console.log("UPC returned non 200 (" + upc_search_result.status.toString() + ")")
-      return false
-    }
-
-    if(upc_search_result.data.items == undefined) {
-      console.log("Empty search results -- bailing")
-      return false
-    }
-
-    if(upc_search_result.data.items.length < 1) {
-      console.log("Empty search results -- bailing")
-      return false
-    }
-
-    return upc_search_result.data.items[0]
-  }
-
-  async read_bigc(code) {
-    let bigc_result
-    try {
-      bigc_result = await axios.get(
-        'https://www.bigc.co.th/sb.php',
-        {
-          params: {
-            q: code,
-            storeid: 1,
-            currencycode: 'THB'
-          },
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'User-agent': 'Mozilla Firefox'
-          }
-        }
-      )
-
-      if(bigc_result.data.response) {
-        if(bigc_result.data.response.numFound > 0) {
-          return bigc_result.data.response.docs[0]
-        }
-      }
-
-      return false
-    } catch(error) {
-      console.log(error)
-      console.log({
-        message: 'read local ...',
-        status: error.response.status,
-        headers: error.response.headers
-      })
-
-      return false
-    }
-
-  }
-
-  async write(item) {
-
-  }
-
-  async processNEW(record) {
-    const handlers = eventHandlerMapping[record.event].map( async (handlerClass) => {
-      let hc = new handlerClass()
-      return hc.run()
-    })
-
-    let results
-    try {
-      results = await Promise.allSettled(handlers)
-      console.log('asdaasd', results)
-    } catch( error ) {
-      console.log(error)
-    }
-  }
-
-  async process(record) {
-    console.log(record.event, record.version, JSON.stringify(record.actor), JSON.stringify(record.payload))
-    // https://world.openfoodfacts.org/api/v0/product/8850999220000.json
-    return
-
-    events_seen.labels(record.event).inc()
-
-    switch(record.event) {
-      case 'ScanCreated':
-        const local = await this.check_local(record.payload.code)
-
-        if(local) {
-          console.log('got code', local)
-          return
-        }
-
-        if(!record.payload.code.match(/^[0-9]+$/)) {
-          console.log("Doesnt look like a UPC")
-          return
-        }
-
-        if(record.payload.code.length < 5 && record.payload.code.length > 16) {
-          console.log("Doesnt look like a UPC")
-          return
-        }
-
-        return
-
-        let bigc = await this.read_bigc(record.payload.code)
-        console.log('bigc result', bigc)
-
-        let item
-        if(bigc) {
-          item = {
-            name: bigc.name_varchar,
-            url: bigc.url_path_varchar
-          }
-        } else {
-          const upcitemdb = this.read_upcitemdb(record.payload.code)
-          item = {
-            name: item.title
-          }
-        }
-
-        item.code = record.payload.code
-        console.log('found', item)
-
-
-        try {
-          const write_response = await axios.post(
-            config.reporter_base_url + '/codes/',
-            item
-          )
-
-          console.log(write_response.data)
-        } catch(error) {
-          console.log(error.response.data)
-        }
-
-        return
-        try {
-  
-        } catch(error) {
-          if(error.response.status === 404) {
-            console.log("Query the internet")
-
-            let upc_search_result
-            try {
-              upc_search_result = await axios.get(
-                'https://api.upcitemdb.com/prod/trial/lookup',
-                {
-                  params: {
-                    upc: record.payload.code
-                  }
-                }
-              )
-
-              console.log(JSON.stringify(upc_search_result.headers, null, 2))
-              console.log(JSON.stringify(upc_search_result.data, null, 2))
-            } catch(error) {
-              console.log("Failed to query the UPC database")
-              return
-            }
-
-            if(upc_search_result.status != 200) {
-              console.log("UPC returned non 200 (" + upc_search_result.status.toString() + ")")
-              return
-            }
-
-            if(upc_search_result.data.items == undefined) {
-              console.log("Empty search results -- bailing")
-              return
-            }
-
-            if(upc_search_result.data.items.length < 1) {
-              console.log("Empty search results -- bailing")
-              return
-            }
-
-            const write_response = await axios.post(
-              config.reporter_base_url + '/codes/',
-              {
-                code: record.payload.code,
-                name: upc_search_result.data.items[0].title
-              }
-            )
-
-            console.log(write_response.data)
-
-          } else {
-            console.log(error)
-          }
-        }
-
-        return
-
-
-
-        console.log({
-          status: upc_search_result.status,
-          headers: upc_search_result.headers
-        })
-        console.log(JSON.stringify(upc_search_result.data, null, 2))
-        break
-
-      default:
-        console.log('doing nothing with this')
-    }
   }
 }
 
