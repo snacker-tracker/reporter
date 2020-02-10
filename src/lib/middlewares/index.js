b59d222fb8d66c27bbeaf0f87bab3e75dabbde08
Laurence MacGuire <leprechaun@gmail.com>
2020-02-10 23:58:51 +0700

    lib/middlewares: forgot to add index, so qa is crashing

---
+++ src/lib/middlewares/index.js
@@ -0,0 +1,11 @@
+import AccessLogs from './AccessLogs'
+import Auth from './Auth'
+import RequestId from './RequestId'
+import ValidateResponse from './ValidateResponse'
+
+export {
+  AccessLogs,
+  Auth,
+  RequestId,
+  ValidateResponse
+}
