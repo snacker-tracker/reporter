f0db20c7c0520ff60142bbaf60a5be3432f5cbcb
Laurence MacGuire <leprechaun@gmail.com>
2020-02-10 16:03:02 +0700

    lib/http/GetOperation: add simple tests

---
+++ src/lib/http/GetOperation.spec.js
@@ -0,0 +1,84 @@
+import GetOperation from './GetOperation'
+
+class TestEventHandler2 {
+  run = jest.fn()
+}
+
+const query = jest.fn().mockReturnThis
+
+const Query = {
+  query: jest.fn().mockReturnThis(),
+  findById: jest.fn().mockReturnThis()
+}
+
+const Model = {
+  query: jest.fn(),
+  findById: jest.fn()
+}
+
+
+class ExampleGetOperation extends GetOperation {
+  static model = Query
+
+  async extract_params(req) {
+    this.args = {
+      id: req.params.id
+    }
+  }
+}
+
+describe(GetOperation, () => {
+  let operation
+  let request
+  beforeEach( () => {
+    Model.query.mockClear()
+    operation = new ExampleGetOperation()
+    request = {
+      params: {
+        id: 'deadbeef-1'
+      },
+      query: {
+        include_meta: false
+      }
+    }
+  })
+
+  describe('behaviour', () => {
+    it('returns an HTTPResponse', async () => {
+      const response = await operation.run(request, {})
+      expect(response.constructor.name).toBe('HTTPResponse')
+    })
+
+    it('queried the model using request.params.id', async () => {
+      await operation.run(request)
+
+      expect(operation.constructor.model.query).toHaveBeenCalled()
+      expect(operation.constructor.model.findById).toHaveBeenCalledWith('deadbeef-1')
+    })
+
+    it('returns a 404 when the resource is null/misconfigured', async () => {
+      operation.resources = () => {
+        return {
+          resource: null
+        }
+      }
+      await operation.run(request)
+
+      expect(operation.constructor.model.query).toHaveBeenCalled()
+      expect(operation.constructor.model.findById).toHaveBeenCalledWith('deadbeef-1')
+    })
+
+    it('returns a 404 when the user gets denied', async () => {
+      operation.requesterCanReadResource = () => {
+        return false
+      }
+
+      const result = await operation.run(request)
+
+      expect(operation.constructor.model.query).toHaveBeenCalled()
+      expect(operation.constructor.model.findById).toHaveBeenCalledWith('deadbeef-1')
+
+      expect(result.status).toBe(404)
+    })
+  })
+})
