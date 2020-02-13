f7cab30a33be89df6fec385db30dabf5d46bd5fb
Laurence MacGuire <leprechaun@gmail.com>
2020-02-13 23:14:12 +0700

    whoops: never git added those tests

---
+++ src/lib/http/CreateOperation.spec.js
@@ -0,0 +1,148 @@
+import CreateOperation from './CreateOperation'
+import logger from '../logger'
+
+import { Model, QueryBuilder } from 'objection'
+
+class TestQueryBuilder extends QueryBuilder {
+  resolve(results) {
+    this._resulter = () => {
+      return results
+    }
+  }
+
+  reject(error) {
+    this._resulter = () => {
+      throw error
+    }
+  }
+
+  then(callback) {
+    callback(this._resulter())
+  }
+}
+
+class MockModel extends Model {
+
+}
+
+class ExampleOperation extends CreateOperation {
+  static canBeCalledAnonymously = true
+  static model = MockModel
+}
+
+const event_publisher = {
+  publish() {}
+}
+
+describe(CreateOperation, () => {
+  let operation
+  let request
+  let queryBuilderProvider
+  let queryResult
+  let queryBuilder
+  let publisher
+
+  beforeEach( () => {
+    operation = new ExampleOperation({ logger, event_publisher })
+    request = {
+      body: {
+        'some-property': 'some value',
+        'another-property': 'another value'
+      }
+    }
+
+    queryBuilder = TestQueryBuilder.forClass(MockModel)
+
+    publisher = jest.spyOn(event_publisher, 'publish')
+    publisher.mockClear()
+
+    MockModel.query = jest.fn()
+    MockModel.query.mockReturnValue(queryBuilder)
+    queryBuilder.resolve({
+      'some-property': 'some value',
+      'another-property': 'another value'
+    })
+  })
+
+
+  describe('behaviour', () => {
+    it('calls Model.insert with the request body', async () => {
+
+      await operation.run(request, null)
+
+      expect(queryBuilder._operations).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            name: 'insertAndFetch'
+          }),
+          expect.objectContaining({
+            name: 'options',
+            args: [
+              { 'operationId': 'ExampleOperation' }
+            ]
+          })
+
+        ])
+      )
+    })
+
+    it('responds a 201 on success', async () => {
+      const response = await operation.run(request, null)
+
+      expect(response.status).toBe(201)
+    })
+
+    it('responds with the inserted object on success', async () => {
+      const response = await operation.run(request, null)
+
+      expect(response.body).toMatchObject({
+        'some-property': 'some value',
+        'another-property': 'another value'
+      })
+    })
+
+
+    it('publishes an event post creation', async () => {
+      const response = await operation.run(request, null)
+      expect(publisher).toHaveBeenCalledWith(
+        'MockModelCreated',
+        expect.objectContaining({
+          'some-property': 'some value',
+          'another-property': 'another value'
+        }),
+        undefined
+      )
+    })
+
+
+    it('fails w/ a 409 if we get a duplicate key error', async () => {
+      queryBuilder.reject({
+        code: '23505'
+      })
+
+      const response = await operation.run(request, null)
+
+      expect(response.status).toBe(409)
+    })
+
+    it('returns a 500 on unhandled exceptions', async () => {
+      queryBuilder.reject({
+        code: 'whatever'
+      })
+
+      const response = await operation.run(request, null)
+
+      expect(response.status).toBe(500)
+    })
+
+    it('does not publish events when the insert fails', async () => {
+      queryBuilder.reject({
+        code: 'whatever'
+      })
+
+      await operation.run(request, null)
+
+      expect(publisher).not.toHaveBeenCalled()
+    })
+  })
+})
---
+++ src/lib/http/DeleteOperation.spec.js
@@ -0,0 +1,123 @@
+import DeleteOperation from './DeleteOperation'
+import logger from '../logger'
+
+import { Model, QueryBuilder } from 'objection'
+
+class TestQueryBuilder extends QueryBuilder {
+  resolve(results) {
+    this._resulter = () => {
+      return results
+    }
+  }
+
+  reject(error) {
+    this._resulter = () => {
+      throw error
+    }
+  }
+
+  then(callback) {
+    callback(this._resulter())
+  }
+}
+
+class MockModel extends Model {
+
+}
+
+class ExampleOperation extends DeleteOperation {
+  static canBeCalledAnonymously = true
+  static model = MockModel
+}
+
+const event_publisher = {
+  publish() {}
+}
+
+describe(DeleteOperation, () => {
+  let operation
+  let request
+  let queryBuilderProvider
+  let queryResult
+  let queryBuilder
+  let publisher
+
+  beforeEach( () => {
+    operation = new ExampleOperation({ logger, event_publisher })
+    request = {
+      params: {
+        id: 'deadbeef'
+      }
+    }
+
+    queryBuilder = TestQueryBuilder.forClass(MockModel)
+
+    publisher = jest.spyOn(event_publisher, 'publish')
+    publisher.mockClear()
+
+    MockModel.query = jest.fn()
+    MockModel.query.mockReturnValue(queryBuilder)
+    queryBuilder.resolve({
+      'id': 'deadbeef'
+    })
+  })
+
+
+  describe('behaviour', () => {
+    it('calls Model.delete with the correct id', async () => {
+      await operation.run(request, null)
+
+      expect(queryBuilder._operations).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            name: 'delete'
+          }),
+          expect.objectContaining({
+            name: 'options',
+            args: [
+              { 'operationId': 'ExampleOperation' }
+            ]
+          })
+
+        ])
+      )
+    })
+
+    it('responds a 201 on success', async () => {
+      const response = await operation.run(request, null)
+
+      expect(response.status).toBe(201)
+    })
+
+    it('publishes an event post creation', async () => {
+      const response = await operation.run(request, null)
+      expect(publisher).toHaveBeenCalledWith(
+        'MockModelDeleted',
+        expect.objectContaining({
+          'id': 'deadbeef'
+        }),
+        undefined
+      )
+    })
+
+    it('returns a 500 on unhandled exceptions', async () => {
+      queryBuilder.reject({
+        code: 'whatever'
+      })
+
+      const response = await operation.run(request, null)
+
+      expect(response.status).toBe(500)
+    })
+
+    it('does not publish events when the delete fails', async () => {
+      queryBuilder.reject({
+        code: 'whatever'
+      })
+
+      await operation.run(request, null)
+
+      expect(publisher).not.toHaveBeenCalled()
+    })
+  })
+})
