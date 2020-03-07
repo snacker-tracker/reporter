3e0eeea513438c0b95b7d1363cd5b65ac3e0a2ee
Laurence MacGuire <leprechaun@gmail.com>
2020-03-07 16:13:26 +0700

    Auth: Add a TokenProvider class for when I add auth to the stream-processor

---
+++ src/lib/TokenProvider.js
@@ -0,0 +1,45 @@
+class TokenProvider {
+  constructor(config, options) {
+    const axios_options = {
+      baseURL: config.issuer
+    }
+
+    this.axios = options.axios.create(axios_options)
+
+    this.endpoints = config.endpoints
+    this.config = config
+
+    this.cache = {
+      expiry: new Date(0),
+      token: false
+    }
+  }
+
+  async getToken() {
+    if(!this.cache.token || new Date() > this.cache.expiry) {
+      const extra = {}
+
+      if(this.config.audience) {
+        extra.audience = this.config.audience
+      }
+
+      const response = await this.axios.post( this.endpoints.token, {
+        client_id: this.config.client_id,
+        client_secret: this.config.client_secret,
+        grant_type: 'client_credentials',
+        ...extra
+      })
+
+      this.cache = {
+        expiry: new Date() + ( 1000 * response.data.expires_in ),
+        token: response.data.access_token
+      }
+
+      return response.data.access_token
+    } else {
+      return this.cache.token
+    }
+  }
+}
+
+export default TokenProvider
---
+++ src/lib/TokenProvider.spec.js
@@ -0,0 +1,176 @@
+import TokenProvider from './TokenProvider'
+import axios from 'axios'
+
+describe(TokenProvider, () => {
+  const config = {
+    issuer: 'https://my-oidc-issuer.com',
+    endpoints: {
+      token: '/oauth/token'
+    },
+    client_id: 'my-client-id',
+    client_secret: 'super-secret-value'
+  }
+
+  let tokenProvider
+  let axios = {}
+
+  beforeEach( () => {
+    axios.post = jest.fn()
+    axios.post.mockResolvedValue({
+      data: {
+        access_token: 'default-token',
+        scope: 'scope1 scope2',
+        expires_in: 86400,
+        token_type: 'Bearer'
+      }
+    })
+
+    axios.post.mockResolvedValueOnce({
+      data: {
+        access_token: 'the-first-token',
+        scope: 'scope1 scope2',
+        expires_in: 86400,
+        token_type: 'Bearer'
+      }
+    })
+
+    axios.create = jest.fn()
+    axios.create.mockReturnValue(axios)
+
+    tokenProvider = new TokenProvider(
+      config, { axios }
+    )
+  })
+
+  describe('configuration', () => {
+    it('sets the right issuer url', async () => {
+      expect(axios.create).toHaveBeenCalledWith(
+        expect.objectContaining({
+          baseURL: config.issuer
+        })
+      )
+    })
+
+    it('makes a call to the configured token endpoint', async () => {
+      await tokenProvider.getToken()
+
+      expect(axios.post).toHaveBeenCalledWith(
+        config.endpoints.token,
+        expect.any(Object)
+      )
+    })
+
+    it('uses the client_credentials grant type', async () => {
+      await tokenProvider.getToken()
+
+      expect(axios.post).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          client_secret: config.client_secret
+        })
+      )
+    })
+
+
+    it('uses the correct client id', async () => {
+      await tokenProvider.getToken()
+
+      expect(axios.post).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          client_id: config.client_id
+        })
+      )
+    })
+
+    it('uses the correct client secret', async () => {
+      await tokenProvider.getToken()
+
+      expect(axios.post).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          client_secret: config.client_secret
+        })
+      )
+    })
+
+    describe('audiences', () => {
+      it('specifies audience if provided', async () => {
+        tokenProvider = new TokenProvider(
+          { ...config, audience: 'my-audience' }, { axios }
+        )
+
+        await tokenProvider.getToken()
+
+        expect(axios.post).toHaveBeenCalledWith(
+          expect.any(String),
+          expect.objectContaining({
+            audience: 'my-audience'
+          })
+        )
+      })
+
+      it('does not provide audience when it is not provided', async () => {
+        await tokenProvider.getToken()
+
+        expect(axios.post).toHaveBeenCalledWith(
+          expect.any(String),
+          expect.not.objectContaining({
+            audience: 'my-audience'
+          })
+        )
+      })
+    })
+  })
+
+  describe('returns', () => {
+    it('returns a token', async () => {
+      const token = await tokenProvider.getToken()
+      expect(token).toBe('the-first-token')
+    })
+
+    it('caches tokens', async () => {
+      await tokenProvider.getToken()
+      const token = await tokenProvider.getToken()
+      expect(token).toBe('the-first-token')
+    })
+
+    describe('cache expiry', () => {
+      it('gets a new token if we never had one', async () => {
+        tokenProvider.cache = {
+          token: false
+        }
+        await tokenProvider.getToken()
+        const token = await tokenProvider.getToken()
+        expect(token).toBe('the-first-token')
+      })
+
+      it('gets a new token if expired', async () => {
+        tokenProvider.cache = {
+          token: 'an-expired-token',
+          expiry: new Date() - 10000
+        }
+
+        await tokenProvider.getToken()
+        const token = await tokenProvider.getToken()
+        expect(token).toBe('the-first-token')
+        expect(axios.post).toHaveBeenCalled()
+      })
+
+      it('gets a new token if expired', async () => {
+        tokenProvider.cache = {
+          token: 'the-first-token',
+          expiry: new Date() + 10000
+        }
+
+        await tokenProvider.getToken()
+        const token = await tokenProvider.getToken()
+        expect(token).toBe('the-first-token')
+        expect(axios.post).not.toHaveBeenCalled()
+      })
+
+
+    })
+
+  })
+})
