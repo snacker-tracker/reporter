8c6dbf6e90cab2bf72b3be4b18610d8fbe758e5c
Laurence MacGuire <leprechaun@gmail.com>
2020-03-16 03:21:01 +0700

    refactor: de-nest all the functions in AWSMock

--- src/utils/AWSMocks.js
+++ src/utils/AWSMocks.js
@@ -1,60 +1,59 @@
-class AWSMock {
-  static mock() {
-    const mock = jest.fn()
-
-    const response = (type, result) => {
-      return {
-        promise: function() {
-          return Promise[type](result)
-        }
-      }
+const response = (type, result) => {
+  return {
+    promise: function() {
+      return Promise[type](result)
     }
-
-    const resolve = (result) => {
-      return response('resolve', result)
-    }
-
-    const reject = (result) => {
-      return response('reject', result)
-    }
-
-    const once = (type) => {
-      return function(result) {
-        this.mockReturnValueOnce(type(result))
-      }
-    }
-
-    const def = (type) => {
-      return function(result) {
-        this.mockReturnValue(type(result))
-      }
-    }
-
-    Object.assign(mock, {
-      'mockResolvedValue': def(resolve),
-      'mockResolvedValueOnce': once(resolve),
-      'mockRejectedValue': def(reject),
-      'mockRejectedValueOnce': once(reject),
-    })
-
-    return mock
   }
 }
 
+const resolve = (result) => {
+  return response('resolve', result)
+}
+
+const reject = (result) => {
+  return response('reject', result)
+}
+
+const once = (type) => {
+  return function(result) {
+    this.mockReturnValueOnce(type(result))
+  }
+}
+
+const def = (type) => {
+  return function(result) {
+    this.mockReturnValue(type(result))
+  }
+}
+
+const build = () => {
+  const mock = jest.fn()
+
+  Object.assign(mock, {
+    'mockResolvedValue': def(resolve),
+    'mockResolvedValueOnce': once(resolve),
+    'mockRejectedValue': def(reject),
+    'mockRejectedValueOnce': once(reject),
+  })
+
+  return mock
+}
+
+
 class Kinesis {
-  listShards = AWSMock.mock()
-  getShardIterator = AWSMock.mock()
-  getRecords = AWSMock.mock()
-  putRecord = AWSMock.mock()
-  putRecords = AWSMock.mock()
+  listShards = build()
+  getShardIterator = build()
+  getRecords = build()
+  putRecord = build()
+  putRecords = build()
 }
 
 class S3 {
-  listBuckets = AWSMock.mock()
-  listObject = AWSMock.mock()
-  getObject = AWSMock.mock()
-  putObject = AWSMock.mock()
-  deleteObject = AWSMock.mock()
+  listBuckets = build()
+  listObject = build()
+  getObject = build()
+  putObject = build()
+  deleteObject = build()
 }
 
 export {
